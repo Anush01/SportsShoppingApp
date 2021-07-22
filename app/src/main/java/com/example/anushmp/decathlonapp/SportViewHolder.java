c12f2b671303662f13df580200771e5a17ef3f33
rohith <rohithrathna916@gmail.com>
2021-07-22 21:23:28 +0530

    sport commit

--- app/src/main/java/com/example/anushmp/decathlonapp/SportViewHolder.java
+++ app/src/main/java/com/example/anushmp/decathlonapp/SportViewHolder.java
@@ -1,4 +1,23 @@
 package com.example.anushmp.decathlonapp;
 
-public class SportViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SportViewHolder extends RecyclerView.ViewHolder {
+
+    private ImageView mivImage;
+    private TextView mtvSportName;
+
+    public SportViewHolder(@NonNull  View itemView) {
+        super(itemView);
+        initViews(itemView);
+    }
+
+    private void initViews(View itemView) {
+    mtvSportName = itemView.findViewById(R.id.)
+    }
 }
