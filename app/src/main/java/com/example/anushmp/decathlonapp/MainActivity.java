0e0f931f99840a468d1f0ebcf1d9f10e5da7345f
rohith <rohithrathna916@gmail.com>
2021-07-22 11:27:34 +0530

    small commit

--- app/src/main/java/com/example/anushmp/decathlonapp/MainActivity.java
+++ app/src/main/java/com/example/anushmp/decathlonapp/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //write stuff here.
-
-
     }
 }
