74ad572971b060b174c7d81eace97e7b411062ca
Prachip9767 <prachip293@gmail.com>
2021-08-20 18:37:53 +0530

    prachi update

--- app/src/main/java/com/example/anushmp/decathlonapp/Activity/AccountActivity.java
+++ app/src/main/java/com/example/anushmp/decathlonapp/Activity/AccountActivity.java
@@ -1,17 +1,21 @@
 package com.example.anushmp.decathlonapp.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.anushmp.decathlonapp.R;
 
 public class AccountActivity extends AppCompatActivity {
+    Button LogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-
+        LogIn=findViewById(R.id.btnLogIn);
+        Intent intent=new Intent(this,Login_Activity.class);
     }
 }
